631a1742081a8347bfb81ab519f01acd265a3df0
Thomas Gläßle <t_glaessle@gmx.de>
2019-03-19 17:58:46 +0100

    Define most metadata in setup.cfg, simplify setup.py

--- setup.py
+++ setup.py
@@ -18,30 +18,11 @@ def exec_file(path):
 
 
 metadata = exec_file('git-punchcard')
-long_description = read_file('README.rst').decode('utf-8')
 
 setup(
-    name=metadata['__title__'],
     version=metadata['__version__'],
-    description=metadata['__summary__'],
-    long_description=long_description,
     author=metadata['__author__'],
     author_email=metadata['__email__'],
     url=metadata['__uri__'],
     license=metadata['__license__'],
-    scripts=['git-punchcard'],
-    install_requires=[
-        'numpy',
-        'matplotlib',
-        'docopt',
-        'pytz',
-    ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Programming Language :: Python :: 3',
-        'License :: Public Domain',
-        'Topic :: Software Development :: Version Control :: Git',
-    ],
 )
